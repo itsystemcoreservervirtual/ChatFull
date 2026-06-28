5558c33d620ce87ef800246e4e76954ec51134c3
itsystemcoreservervirtual <it.system.core@gmail.com>
2026-06-27 20:58:45 -0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,26 +1,17 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
         unique: true, 
-        trim: true
+        trim: true,
+        lowercase: true 
     },
     password: {
         type: String,
-        required: true 
-    },
-    avatar: {
-        type: String,
-        default: 'https://via.placeholder.com/150' 
-    },
-    blockedUsers: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User' 
-    }]
-}, {
-    timestamps: true 
-});
+        required: true
+    }
+}, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', UserSchema);
